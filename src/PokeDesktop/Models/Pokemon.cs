373bebe7066e6d1377aa931dcac974f1d8475b3c
Илья Глущук <mrhide5010@gmail.com>
2026-07-02 04:02:24 +0300

    Create Pokemon model

---
+++ src/PokeDesktop/Models/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeDesktop.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public List<PokemonType> Types { get; set; } = new();
+        public Sprites Sprites { get; set; } = new();
+    }
+
+    public class PokemonType
+    {
+        public TypeInfo Type { get; set; } = new();
+    }
+
+    public class TypeInfo
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class Sprites
+    {
+        public string FrontDefault { get; set; } = string.Empty;
+    }
+}
