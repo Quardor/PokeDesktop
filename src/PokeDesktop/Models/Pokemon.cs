3f88dfd2416cd9cefa91ba8c8e5ee96b9bbba33d
Илья Глущук <mrhide5010@gmail.com>
2026-07-02 12:46:06 +0300

    fix model for new API

--- src/PokeDesktop/Models/Pokemon.cs
+++ src/PokeDesktop/Models/Pokemon.cs
@@ -1,31 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
 
 namespace PokeDesktop.Models
 {
-    internal class Pokemon
+    public class Pokemon
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public int Height { get; set; }
-        public int Weight { get; set; }
-        public List<PokemonType> Types { get; set; } = new();
-        public Sprites Sprites { get; set; } = new();
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("formId")]
+        public string FormId { get; set; }
+
+        [JsonProperty("dexNr")]
+        public int DexNr { get; set; }
+
+        [JsonProperty("generation")]
+        public int Generation { get; set; }
+
+        [JsonProperty("names")]
+        public Names Names { get; set; }
+
+        [JsonProperty("stats")]
+        public Stats Stats { get; set; }
+
+        [JsonProperty("primaryType")]
+        public PokemonType PrimaryType { get; set; }
+
+        [JsonProperty("secondaryType")]
+        public PokemonType SecondaryType { get; set; }
+
+        [JsonProperty("quickMoves")]
+        public object QuickMoves { get; set; }
+
+        [JsonProperty("cinematicMoves")]
+        public object CinematicMoves { get; set; }
+
+        [JsonProperty("eliteQuickMoves")]
+        public object EliteQuickMoves { get; set; }
+
+        [JsonProperty("eliteCinematicMoves")]
+        public object EliteCinematicMoves { get; set; }
+
+        [JsonProperty("assets")]
+        public Assets Assets { get; set; }
     }
 
-    internal class PokemonType
+    public class Names
     {
-        public TypeInfo Type { get; set; } = new();
+        public string English { get; set; }
     }
 
-    internal class TypeInfo
+    public class Stats
     {
-        public string Name { get; set; } = string.Empty;
+        public int Stamina { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
     }
 
-    internal class Sprites
+    public class PokemonType
     {
-        public string FrontDefault { get; set; } = string.Empty;
+        [JsonProperty("type")]
+        public string TypeName { get; set; }
+
+        [JsonProperty("names")]
+        public Names Names { get; set; }
+    }
+
+    public class Move
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("power")]
+        public int Power { get; set; }
+
+        [JsonProperty("energy")]
+        public int Energy { get; set; }
+
+        [JsonProperty("durationMs")]
+        public int DurationMs { get; set; }
+
+        [JsonProperty("names")]
+        public Names Names { get; set; }
+    }
+
+    public class Assets
+    {
+        public string Image { get; set; }
+        public string ShinyImage { get; set; }
     }
 }
